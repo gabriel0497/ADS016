7f46551df9217e2aaa4441cb4f5f6800aff4e98c
Paulo Cezar <Paulo Cezar@INSPIRON1545>
2019-03-27 21:07:16 -0300

    Criando projeto Atividade03

---
+++ Atividade03/public_html/scripts.js
@@ -0,0 +1,17 @@
+
+
+
+//alert ('dsds');
+
+
+
+
+function validar (){
+    
+    //var nome = document.getElementById('lastname');
+    
+    
+    alert(document.getElementById('lastname'));
+}
+
+
